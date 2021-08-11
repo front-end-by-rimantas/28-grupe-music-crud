8a9b19180d0d68a3f08d48b239e3b3e119b32f66
Rimantas <belauzas@yahoo.com>
2021-08-11 20:11:48 +0300

    Solution

--- Artist.js
+++ Artist.js
@@ -1,6 +1,51 @@
 class Artist {
-    constructor() {
+    constructor(artistName, currency) {
+        this.artistName = artistName;
+        this.currency = currency;
+        this.pricePerSong = 0;
+        this.wallet = 0;
+        this.list = [];
+    }
 
+    intro() {
+        console.log(`Hi, my name is ${this.artistName} and I am a musician!`);
+    }
+
+    songPrice(newPrice) {
+        this.pricePerSong = newPrice;
+        const price = this.formatedMoney(newPrice);
+        console.log(`${this.artistName}'s price per song is ${price}`);
+    }
+
+    addSong(songName) {
+        this.list.push({
+            name: songName,
+            playCount: 0,
+        })
+    }
+
+    playlist() {
+        console.log(`${this.artistName}'s playlist:`);
+        console.log(`====================`);
+        let number = 0;
+        for (const song of this.list) {
+            console.log(`${++number}. ${song.name} (${song.playCount})`);
+        }
+    }
+
+    playSong(songIndex) {
+        const song = this.list[songIndex];
+        song.playCount++;
+        this.wallet += this.pricePerSong;
+        console.log(`Playing song: ${song.name}.`);
+    }
+
+    fortune() {
+        console.log(`Total lifetime wealth of Raimonda is ${this.formatedMoney(this.wallet)} right now!`);
+    }
+
+    formatedMoney(cents) {
+        return (cents / 100).toFixed(2) + ' ' + this.currency;
     }
 }
 
